d338fdab9e252860693e885c6f654d6a3b30267f
tae-uk-k <110802129+tae-uk-k@users.noreply.github.com>
2024-11-01 22:56:40 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T, N, M;
+    cin >> T;
+    
+    for (int i = 0; i < T; i++) {
+        cin >> N;
+        vector<int> coins(N);
+        
+        for (int j = 0; j < N; j++) {
+            cin >> coins[j];
+        }
+        
+        cin >> M;
+        vector<int> dp(M + 1, 0); 
+        dp[0] = 1;  
+
+        for (int coin : coins) {  
+            for (int k = coin; k <= M; k++) { 
+                dp[k] += dp[k - coin];
+            }
+        }
+
+        cout << dp[M] << "\n"; 
+    }
+    
+    return 0;
+}
