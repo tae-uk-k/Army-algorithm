576e396c09bf4e3ec5f790213d53995dbe8cc6ab
tae-uk-k <110802129+tae-uk-k@users.noreply.github.com>
2024-11-22 23:37:11 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+    cout<<1-a<<' ';
+    cin>>a;
+    cout<<1-a<<' ';
+    cin>>a;
+    cout<<2-a<<' ';
+    cin>>a;
+    cout<<2-a<<' ';
+    cin>>a;
+    cout<<2-a<<' ';
+    cin>>a;
+    cout<<8-a<<' ';
+}
