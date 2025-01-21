8f09ca4b2770ca9ebfdbfb26c20ca6be19e8749e
tae-uk-k <110802129+tae-uk-k@users.noreply.github.com>
2025-01-21 22:55:07 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> s;
+
+void bfs(vector<vector<int>> &space, vector<vector<bool>> &visited,int a,int b) {
+    int sum=1;
+    queue<pair<int, int>> q;
+    q.push({a, b});
+    visited[a][b] = true;
+
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        if (x + 1 <= space.size() - 1 && space[x + 1][y] == 1 && !visited[x + 1][y]) {
+            visited[x + 1][y] = true;
+            sum++;
+            q.push({x + 1, y});
+        }
+        if (x - 1 >= 1 && space[x - 1][y] == 1 && !visited[x - 1][y]) {
+            visited[x - 1][y] = true;
+            sum++;
+            q.push({x - 1, y});
+        }
+        if (y + 1 <= space[0].size() - 1 && space[x][y + 1] == 1 && !visited[x][y + 1]) {
+            visited[x][y + 1] = true;
+            sum++;
+            q.push({x, y + 1});
+        }
+        if (y - 1 >= 1 && space[x][y - 1] == 1 && !visited[x][y - 1]) {
+            visited[x][y - 1] = true;
+            sum++;
+            q.push({x, y - 1});
+        }
+    }
+    s.push_back(sum);
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int N, M;
+    cin >> N;
+    M=N;
+    vector<vector<int>> space(N + 1, vector<int>(M + 1, 0));
+    vector<vector<bool>> visited(N + 1, vector<bool>(M + 1, false));
+
+    for (int i = 1; i <= N; i++) {
+        string row;
+        cin >> row;
+        for (int j = 1; j <= M; j++) {
+            space[i][j] = row[j - 1] - '0';
+        }
+    }
+    for(int i=1; i<=N; i++){
+        for (int j = 1; j <= M; j++){
+            if(visited[i][j]!=true&&space[i][j]==1){
+                bfs(space, visited,i,j);
+            }
+        }
+    }
+    sort(s.begin(),s.end());
+    cout<<s.size()<<"\n";
+    for(int k : s){
+        cout<<k<<"\n";
+    }
+    return 0;
+}
+
+
+
+
+
+
+
