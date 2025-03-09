53c7ecb92f36105acc2aa8cccafc2cf65523f153
tae-uk-k <110802129+tae-uk-k@users.noreply.github.com>
2025-03-09 09:47:42 +0900

    [Bronze V] Title: 홍익대학교, Time: 168 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+	    Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = 1946;
+		System.out.print(a - b);
+	}
+}
