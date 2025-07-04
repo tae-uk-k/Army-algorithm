4c2d83fbfbc18bd4418284e9a2e12768db029430
tae-uk-k <110802129+tae-uk-k@users.noreply.github.com>
2025-07-04 23:14:31 +0900

    [Gold II] Title: 문제집, Time: 28 ms, Memory: 4216 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> E(N + 1);
+    vector<int> indegree(N + 1, 0);
+
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        E[a].push_back(b);
+        indegree[b]++;
+    }
+
+    priority_queue<int, vector<int>, greater<int>>q;
+    vector<int> ans;
+    for (int i = 1; i <= N; i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+        int u = q.top(); q.pop();
+        ans.push_back(u);
+        for (int v : E[u]) {
+            indegree[v]--;
+            if (indegree[v] == 0) {
+                q.push(v);
+            }
+        }
+    }
+
+    for (int x : ans) {
+        cout << x << ' ';
+    }
+        
+
+
+    return 0;
+}
